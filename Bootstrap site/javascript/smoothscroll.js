3acb947dd38ce9f4be0e60501e47e66d37d2bd3b
Matt Jones <MtlJ1991@gmail.com>
2017-10-30 17:19:37 +0000

    Website October 2017. Smooth scroll added.
    Responsiveness needs work, as does the footer. Minor tweaks to the carousel needed. Everything else is working well.

---
+++ Bootstrap site/javascript/smoothscroll.js
@@ -0,0 +1,24 @@
+/**
+ * SmoothScroll
+ * This helper script created by DWUser.com.  Copyright 2012 DWUser.com.  
+ * Dual-licensed under the GPL and MIT licenses.  
+ * All individual scripts remain property of their copyrighters.
+ * Date: 10-Sep-2012
+ * Version: 1.0.1
+ */
+$(document).ready(function(){
+    $('a[href*=#]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body')
+          .animate({scrollTop: targetOffset}, 1000);
+         return false;
+        }
+      }
+    });
+  });
